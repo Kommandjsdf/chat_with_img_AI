8c9f35bf325b551466b524a8c79f731043c101b2
KsiuTretyakova <hrytsenkoks@gmail.com>
2025-02-15 11:30:31 +0200

    Fixed bugs: static and objects

--- app/views.py
+++ app/views.py
@@ -3,5 +3,5 @@ from .models import Contact
 
 # Create your views here.
 def contact_list(request):
-    contacts = Contact.object.all()
-    return render(request, 'app/contact_list.html', {'contacts': contacts})
+    contacts = Contact.objects.all()
+    return render(request, 'contact_list.html', {'contacts': contacts})
