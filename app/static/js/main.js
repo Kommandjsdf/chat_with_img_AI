02e26ed5ac0bf2cb76074add8a86f5476b4ed030
yar_shto <jarik13012000@gmail.com>
2025-04-19 11:35:28 +0100

    main.js

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,16 +1,16 @@
 //app/static/js/main.js
 document.addEventListener("DOMContentLoaded", function() {
 
-//  window.toggleDetails = function(row) {
-//    const contactId = row.dataset.contactId;
-//    const detailsRow = document.getElementById('details-' + contactId);
-//
-//    if (detailsRow.style.display === 'none') {
-//        detailsRow.style.display === 'table-row';
-//    } else {
-//        detailsRow.style.display === 'none';
-//    }
-//  }
+  window.toggleDetails = function(row) {
+    const contactId = row.dataset.contactId;
+    const detailsRow = document.getElementById('details-' + contactId);
+
+    if (detailsRow.style.display === 'none') {
+        detailsRow.style.display = 'table-row';
+    } else {
+        detailsRow.style.display = 'none';
+    }
+  }
 
 
   let selectedRow = null;
@@ -33,17 +33,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
     });
 
-    row.addEventListener("dblclick", function() {
-        const contactId = row.dataset.contactId;
-        const detailsRow = document.getElementById('details-' + contactId);
-
-        if (detailsRow.style.display === 'none') {
-            detailsRow.style.display === 'table-row';
-        } else {
-            detailsRow.style.display === 'none';
-        }
-    }
-    }
+//    row.addEventListener("dblclick", function() {
+//        const contactId = row.dataset.contactId;
+//        const detailsRow = document.getElementById('details-' + contactId);
+//
+//        if (detailsRow.style.display === 'none') {
+//            detailsRow.style.display === 'table-row';
+//        } else {
+//            detailsRow.style.display === 'none';
+//        }
+//    }
+//    }
 
   });
 
